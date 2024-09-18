2638f499f70088e4d7deed9d89cb2694f1f6bd46
Klindziuk <kklindziuk@gmail.com>
2024-09-18 18:15:31 +0200

    Task about includes for string

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,13 +1,4 @@
 // code
-let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
+let str = "zzzzyzz";
 
-switch (true) {
-    case  rule.length < 25:
-      console.log('Все таки нет правил без исключения');
-      break;
-    case rule.length > 25:
-        console.log('У меня 100 пудов все получится');
-        break;
-    default:
-        console.log('50 на 50');
-}
+str.includes("y") ? console.log("нашел") : console.log("заблудился");
