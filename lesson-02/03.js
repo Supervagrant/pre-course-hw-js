73d7d6bf15515d0921defdb6833e5f19572ec4d9
Klindziuk <kklindziuk@gmail.com>
2024-09-26 19:19:40 +0200

    Another attempt

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -6,4 +6,5 @@ const giveMeRightNumbers = (value1, value2, maximum) => {
         console.log('Something wrong!');
     }
 }
-giveMeRightNumbers(300, 100, 200)
+giveMeRightNumbers(100, 100, 200);
+giveMeRightNumbers(300, 100, 200);
