14873c12a4f115889e8d67b50f12dc51569da98c
Klindziuk <kklindziuk@gmail.com>
2024-09-26 20:00:26 +0200

    Add 5 task 2 homework

---
+++ lesson-02/05.js
@@ -0,0 +1,11 @@
+// code
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let copyPassport = {...passport};
+
+copyPassport.name = 'Ivan';
+console.log(passport.name);
+console.log(copyPassport.name);
