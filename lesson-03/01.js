74dd2688fd5757a1cf9c0d5e80b152bd86681346
Klindziuk <kklindziuk@gmail.com>
2024-10-01 17:48:47 +0200

    Home 3 task 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const newColors = colors.filter(color => color === "черный" || color === 
+        "красный" || color === "желтый"
+    );
+    return colorsString = newColors.join("-");
+}
+
+console.log(createColorString());
